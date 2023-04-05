6fc6518692d51164eccb1104478ceaba00268cf4
mirrorhanyu <mirrorhanyu@gmail.com>
2023-04-06 02:45:58 +0800

    add the chats components and select chat feature

--- apps/chatroom-frontend/src/components/chatroom/Chatroom.tsx
+++ apps/chatroom-frontend/src/components/chatroom/Chatroom.tsx
@@ -1,14 +1,15 @@
 import './Chatroom.css'
 import Chats from "./chats/Chats";
-import React from "react";
+import React, { useState } from "react";
 import Chat from "./chat/Chat";
 import useChatsQuery from "../../hooks/useChatsQuery";
 
 function Chatroom() {
+  const [activeChat, setActiveChat] = useState<number>(-1)
   const { chats } = useChatsQuery("642d04fdbd473f3c5434a4d9")
   return (
     <div className="chatroom">
-      <Chats chats={chats}/>
+      <Chats chats={chats} activeChat={activeChat} onChatSelected={(index: number) => setActiveChat(index)}/>
       <Chat />
     </div>
   )
--- apps/chatroom-frontend/src/components/chatroom/chats/Chats.tsx
+++ apps/chatroom-frontend/src/components/chatroom/chats/Chats.tsx
@@ -1,15 +1,45 @@
-import './Chats.css'
-import { ChatType } from "../../../types/Chat";
+import "./Chats.css";
+import { ChatType, MemberType, MessageType } from "../../../types/Chat";
 
-function Chats({ chats }: { chats: ChatType[] }) {
+function Chats({
+                 chats,
+                 activeChat,
+                 onChatSelected
+               }: { chats: ChatType[], activeChat: number, onChatSelected: Function }) {
   return (
     <div className="chats">
+      <div className="search">Search</div>
       {
-        chats.map(function(chat){
-          return <div key={chat.id}>{chat.name}</div>
-      })
+        chats.map(function(chat, index) {
+          return <ChatItem key={chat.id} active={activeChat === index} name={chat.name} members={chat.members}
+                           messages={chat.messages} onChatSelected={() => onChatSelected(index)} />;
+        })
       }
     </div>
-  )
+  );
 }
-export default Chats
+
+function ChatItem({
+                    active,
+                    name,
+                    members,
+                    messages,
+                    onChatSelected
+                  }: { active: boolean, name: string, members: MemberType[], messages: MessageType[], onChatSelected: Function }) {
+  const latestMessageTimestamp = new Date(messages[messages.length - 1].createdAt);
+  const hours = latestMessageTimestamp.getHours();
+  const minutes = latestMessageTimestamp.getMinutes();
+  const formattedTime = `${hours}:${minutes < 10 ? "0" + minutes : minutes}`;
+  return (
+    <div className="chat-item" onClick={() => onChatSelected()} style={{ backgroundColor: active ? '#26252D' : '#1D1C21' }}>
+      <img src={members[0].avatar} alt="Chat Image" />
+      <div className="chat-info">
+        <span className="chat-name">{name}</span>
+        <span className="chat-content">{messages[messages.length - 1].content}</span>
+      </div>
+      <span className="latest-message-timestamp">{formattedTime}</span>
+    </div>
+  );
+}
+
+export default Chats;
